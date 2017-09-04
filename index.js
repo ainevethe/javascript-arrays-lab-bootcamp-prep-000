d8af93632672fd075581a745a1daa69e0d92778a
Crista Cooper <aine3d@gmail.com>
2017-09-04 23:00:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = "I don't do much."
 
+var kittens = ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendKitten(name) {
-  var name = [name, name.push]
-  return name
+  kittens.push(name)
+  return kittens
 }
