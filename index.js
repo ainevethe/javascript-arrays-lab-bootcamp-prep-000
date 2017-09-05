785ae07cc66166f6ca96ad8f806937ce16f5401d
Crista Cooper <aine3d@gmail.com>
2017-09-05 00:56:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,8 @@ function destructivelyRemoveFirstKitten(name) {
   kittens.shift(name)
   return kittens
 }
+
+function appendKitten(name) {
+  var newarray = [kittens, ...newkitten]
+  return newarray
+}
